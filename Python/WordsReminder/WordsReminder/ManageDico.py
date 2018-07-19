ef7f05d3ea05b2a0d9787e384f03f8d1a3dc3100
Piercuta <pierre.courteille@gmail.com>
2018-07-19 18:44:49 +0200

    first commit word reminder software

---
+++ Python/WordsReminder/WordsReminder/ManageDico.py
@@ -0,0 +1,23 @@
+import os
+import pickle
+
+dicoFileName = "monthly_dictionnary"
+
+def recup_dico():
+    if os.path.exists(dicoFileName):
+        monthlyDictionnary = open(dicoFileName, "rb")
+        myDeplicker = pickle.Unpickler(monthlyDictionnary)
+        myDico = myDeplicker.load()
+        monthlyDictionnary.close()
+    else:
+        myDico = {}
+    return myDico
+
+def save_dico(dico):
+     monthlyDictionnary = open(dicoFileName, "wb")
+     myDeplicker = pickle.Pickler(monthlyDictionnary)
+     myDeplicker.dump(dico)
+     monthlyDictionnary.close()
+
+
+
---
+++ Python/WordsReminder/WordsReminder/WordsReminder.py
@@ -0,0 +1,35 @@
+import sys
+import time
+from ManageDico import *
+
+
+if sys.argv[1] == "AddDefinition":
+
+    monthlyDico = recup_dico()
+    addDef = True
+    while addDef: 
+        var = input("Tapez un mot puis sa définition en les séparant de deux points : \n")
+        confirmation = input("confirmez vous ce mot et sa définition ? y or n \n")
+        while confirmation == 'n':
+            var = input("Tapez un mot puis sa définition en les séparant de deux points \n")
+            confirmation = input("confirmez vous ce mot et sa définition ? y or n \n")
+        my_new_def = var.split(':')
+        monthlyDico[my_new_def[0]] = my_new_def[1]
+        reply = input("voulez vous ajouter une définition ? y or n \n")
+        if reply == "n":
+            addDef = False
+    
+    save_dico(monthlyDico)
+
+if sys.argv[1] == "ShowMonthlyDefinition":
+    monthlyDico = recup_dico()
+    if len(monthlyDico) != 0:
+        for key,value in monthlyDico.items():
+            print (key + " : " + value + "\n\n")
+            time.sleep(1)
+    else:
+        print("pas de définition ce mois-ci")
+
+if sys.argv[1] == "ShowAllRegisteredDefinition":
+    print ("option 3")
+
