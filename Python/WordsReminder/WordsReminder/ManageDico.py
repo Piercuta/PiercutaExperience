a0821f5ad1044b47e3eb87e227c4fee76ca592be
Piercuta <pierre.courteille@gmail.com>
2018-07-20 12:54:13 +0200

    modification of word reminder program

--- Python/WordsReminder/WordsReminder/ManageDico.py
+++ Python/WordsReminder/WordsReminder/ManageDico.py
@@ -1,23 +1,49 @@
 import os
 import pickle
+import datetime
 
-dicoFileName = "monthly_dictionnary"
+my_monthly_dico_filename = "monthly_dictionnary"
+my_full_dico_filename = "full_dictionnary"
 
-def recup_dico():
-    if os.path.exists(dicoFileName):
-        monthlyDictionnary = open(dicoFileName, "rb")
-        myDeplicker = pickle.Unpickler(monthlyDictionnary)
-        myDico = myDeplicker.load()
-        monthlyDictionnary.close()
+def recup_dico(dico_filename):
+    """function to get the dictionnary (accordinghis name) from binary file"""
+    if os.path.exists(dico_filename):
+        binary_dictionnary = open(dico_filename, "rb")
+        my_deplicker = pickle.Unpickler(binary_dictionnary)
+        my_dico = my_deplicker.load()
+        binary_dictionnary.close()
     else:
-        myDico = {}
-    return myDico
+        my_dico = Dictionnary()
+    return my_dico
 
-def save_dico(dico):
-     monthlyDictionnary = open(dicoFileName, "wb")
-     myDeplicker = pickle.Pickler(monthlyDictionnary)
-     myDeplicker.dump(dico)
-     monthlyDictionnary.close()
+def save_dico(dico_filename,my_new_dico):
+    """function to save the dictionnary as a binary file"""
+    binary_dictionnary = open(dico_filename, "wb")
+    my_deplicker = pickle.Pickler(binary_dictionnary)
+    my_deplicker.dump(my_new_dico)
+    binary_dictionnary.close()
+
+def def_and_confirmation():
+    """function to interact with the user asking a definition and a confirmation
+    it returns a tuple"""
+    definition = input("Tapez un mot puis sa définition en les séparant de deux points : \n")
+    confirmation = input("confirmez vous ce mot et sa définition ? y or n \n")
+    return (definition , confirmation)
 
 
+class Dictionnary:
+    def __init__(self):
+        # date of dictionnary creation
+        self.datetime = datetime.datetime.now()
+        self.dictionnary = {}
 
+    def display_all_definition(self):
+        if len(self.dictionnary) != 0:
+            for key,value in self.dictionnary.items():
+                print (key + " : " + value + "\n\n")
+        else:
+            print("pas de définition dans le dictionnaire")
+
+    def add_dictionnary(self, monthlyDico):
+        self.dictionnary.update(monthlyDico)
+    
--- Python/WordsReminder/WordsReminder/WordsReminder.py
+++ Python/WordsReminder/WordsReminder/WordsReminder.py
@@ -1,35 +1,46 @@
 import sys
+import datetime
 import time
 from ManageDico import *
 
 
 if sys.argv[1] == "AddDefinition":
+        
+    my_monthly_dico = recup_dico(my_monthly_dico_filename)
+    if(my_monthly_dico.datetime.month != datetime.datetime.now().month):
+        my_monthly_dico = Dictionnary()
+    my_full_dico = recup_dico(my_full_dico_filename)
 
-    monthlyDico = recup_dico()
-    addDef = True
-    while addDef: 
-        var = input("Tapez un mot puis sa définition en les séparant de deux points : \n")
-        confirmation = input("confirmez vous ce mot et sa définition ? y or n \n")
+    add_def = True
+    while add_def:
+        confirmation = 'n'
         while confirmation == 'n':
-            var = input("Tapez un mot puis sa définition en les séparant de deux points \n")
-            confirmation = input("confirmez vous ce mot et sa définition ? y or n \n")
-        my_new_def = var.split(':')
-        monthlyDico[my_new_def[0]] = my_new_def[1]
-        reply = input("voulez vous ajouter une définition ? y or n \n")
-        if reply == "n":
-            addDef = False
-    
-    save_dico(monthlyDico)
+            definition, confirmation = def_and_confirmation()
+            my_new_def = definition.split(':')
+            my_monthly_dico.dictionnary[my_new_def[0]] = my_new_def[1]
+            reply = input("voulez vous ajouter une définition ? y or n \n")
+            if reply == "n":
+                add_def = False   
+
+    save_dico(my_monthly_dico_filename, my_monthly_dico)
+    my_full_dico.add_dictionnary(my_monthly_dico.dictionnary)
+    save_dico(my_full_dico_filename, my_full_dico)
 
 if sys.argv[1] == "ShowMonthlyDefinition":
-    monthlyDico = recup_dico()
-    if len(monthlyDico) != 0:
-        for key,value in monthlyDico.items():
+    my_monthly_dico = recup_dico(my_monthly_dico_filename)
+    if len(my_monthly_dico.dictionnary) != 0:
+        for key,value in my_monthly_dico.dictionnary.items():
             print (key + " : " + value + "\n\n")
             time.sleep(1)
     else:
         print("pas de définition ce mois-ci")
 
 if sys.argv[1] == "ShowAllRegisteredDefinition":
-    print ("option 3")
+    my_full_dico = recup_dico(my_full_dico_filename)
+    if len(my_full_dico.dictionnary) != 0:
+        for key,value in my_full_dico.dictionnary.items():
+            print (key + " : " + value + "\n\n")
+            time.sleep(1)
+    else:
+        print("pas de définition dans le diciotnnaire")
 
