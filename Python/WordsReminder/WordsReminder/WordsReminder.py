adea96eb4e04221a230fc38da9190c73945b987f
Piercuta <pierre.courteille@gmail.com>
2018-07-21 18:07:37 +0200

    argparse added and def main function

--- Python/WordsReminder/WordsReminder/WordsReminder.py
+++ Python/WordsReminder/WordsReminder/WordsReminder.py
@@ -3,51 +3,63 @@ import sys
 import datetime
 import time
 from ManageDico import *
+import argparse
 
+parser = argparse.ArgumentParser(description='This my first program with argument')
+parser.add_argument('-ad','--addDefinition', action='store_true',help='add a definition')
+parser.add_argument('-smd','--showMonthlyDefinition', action='store_true' ,help='show monthly definition')
+parser.add_argument('-sad','--showAllDefinition', action='store_true', help='show all definition')
+parser.add_argument('-gd','--generateDictionnary', nargs = 1, help='generate the txt dictionnar file')
+args = parser.parse_args()
 
-if sys.argv[1] == "AddDefinition":
-        
-    my_monthly_dico = recup_dico(my_monthly_dico_filename)
-    if(my_monthly_dico.datetime.month != datetime.datetime.now().month):
-        my_monthly_dico = Dictionnary()
-    my_full_dico = recup_dico(my_full_dico_filename)
+def main(args):
+    if args.addDefinition:      
+        my_monthly_dico = recup_dico(my_monthly_dico_filename)
+        if(my_monthly_dico.datetime.month != datetime.datetime.now().month):
+            my_monthly_dico = Dictionnary()
+        my_full_dico = recup_dico(my_full_dico_filename)
 
-    add_def = True
-    while add_def:
-        confirmation = 'n'
-        while confirmation == 'n':
-            definition, confirmation = def_and_confirmation()
-            my_new_def = definition.split(':')
-            my_monthly_dico.dictionnary[my_new_def[0]] = my_new_def[1]
-            reply = input("voulez vous ajouter une définition ? y or n \n")
-            if reply == "n":
-                add_def = False   
+        add_def = True
+        while add_def:
+            confirmation = 'n'
+            while confirmation == 'n':
+                definition, confirmation = def_and_confirmation()
+                my_new_def = definition.split(':')
+                my_monthly_dico.dictionnary[my_new_def[0]] = my_new_def[1]
+                reply = input("voulez vous ajouter une définition ? y or n \n")
+                if reply == "n":
+                    add_def = False   
 
-    save_dico(my_monthly_dico_filename, my_monthly_dico)
-    my_full_dico.add_dictionnary(my_monthly_dico.dictionnary)
-    save_dico(my_full_dico_filename, my_full_dico)
+        save_dico(my_monthly_dico_filename, my_monthly_dico)
+        my_full_dico.add_dictionnary(my_monthly_dico.dictionnary)
+        save_dico(my_full_dico_filename, my_full_dico)
 
-if sys.argv[1] == "ShowMonthlyDefinition":
-    my_monthly_dico = recup_dico(my_monthly_dico_filename)
-    if len(my_monthly_dico.dictionnary) != 0:
-        for key,value in sorted(my_monthly_dico.dictionnary.items()):
-            print ('{:<14}  {:<14}\n'.format( key, value))
-            time.sleep(2)
-    else:
-        print("pas de définition ce mois-ci")
+    if args.showMonthlyDefinition:
+        my_monthly_dico = recup_dico(my_monthly_dico_filename)
+        if len(my_monthly_dico.dictionnary) != 0:
+            for key,value in sorted(my_monthly_dico.dictionnary.items()):
+                print ('{:<14}  {:<14}\n'.format( key, value))
+                time.sleep(2)
+        else:
+            print("pas de définition ce mois-ci")
 
-if sys.argv[1] == "ShowAllRegisteredDefinition":
-    my_full_dico = recup_dico(my_full_dico_filename)
-    if len(my_full_dico.dictionnary) != 0:
-        for key,value in sorted(my_full_dico.dictionnary.items()):
-            print ('{:<14}  {:<14}\n'.format( key, value))
-            time.sleep(1)
-    else:
-        print("pas de définition dans le diciotnnaire")
+    if args.showAllDefinition:
+        my_full_dico = recup_dico(my_full_dico_filename)
+        if len(my_full_dico.dictionnary) != 0:
+            for key,value in sorted(my_full_dico.dictionnary.items()):
+                print ('{:<14}  {:<14}\n'.format( key, value))
+                time.sleep(1)
+        else:
+            print("pas de définition dans le diciotnnaire")
 
-if sys.argv[1] == "PrintDictionnary":
-    dico_name = sys.argv[2]
-    my_dico = recup_dico(dico_name)  
-    with open(dico_name + '.txt', 'w') as file:
-        for key,value in sorted(my_dico.dictionnary.items()):
-            file.write ('{:<14}  {:<14}\n'.format( key, value))
+    if args.generateDictionnary:
+        dico_name = args.generateDictionnary[0]
+        my_dico = recup_dico(dico_name)  
+        with open(dico_name + '.txt', 'w') as file:
+            for key,value in sorted(my_dico.dictionnary.items()):
+                file.write ('{:<14}  {:<14}\n'.format( key, value))
+    pass
+
+if __name__ == "__main__":
+    main(args)
+
