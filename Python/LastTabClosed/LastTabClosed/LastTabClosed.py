94d7f81dc11f7e01aeecaa1c7bbc0bcd632f8e95
Piercuta <pierre.courteille@gmail.com>
2018-07-25 17:03:25 +0200

    add if __name__ == "__main__":     main()

--- Python/LastTabClosed/LastTabClosed/LastTabClosed.py
+++ Python/LastTabClosed/LastTabClosed/LastTabClosed.py
@@ -7,23 +7,27 @@ import sqlite3
 import webbrowser
 import time
 
-db_file_location = r'C:\Users\pierr\AppData\Roaming\Mozilla\Firefox\Profiles\o4eve06y.default\places.sqlite'
-conn = sqlite3.connect(db_file_location)
-cursor = conn.cursor()
-cursor.execute("""SELECT url, last_visit_date FROM moz_places""")
-history = cursor.fetchall()
-conn.close()
+def main():
+	db_file_location = r'C:\Users\pierr\AppData\Roaming\Mozilla\Firefox\Profiles\o4eve06y.default\places.sqlite'
+	conn = sqlite3.connect(db_file_location)
+	cursor = conn.cursor()
+	cursor.execute("""SELECT url, last_visit_date FROM moz_places""")
+	history = cursor.fetchall()
+	conn.close()
 
-history_sorted_by_last_visit_date = sorted(history, key = lambda tup: 0 if tup[1] is None else tup[1])
-history_sorted_by_last_visit_date.reverse()
+	history_sorted_by_last_visit_date = sorted(history, key = lambda tup: 0 if tup[1] is None else tup[1])
+	history_sorted_by_last_visit_date.reverse()
 
-url_list = []
-i=0
-while i <5:
-    url_list.append(history_sorted_by_last_visit_date[i][0])
-    i += 1
+	url_list = []
+	i=0
+	while i <5:
+		url_list.append(history_sorted_by_last_visit_date[i][0])
+		i += 1
 
-# open defaut browser with a list of url 
-for url in url_list:
-    webbrowser.open_new_tab(url)
-    time.sleep(1)
+	# open defaut browser with a list of url 
+	for url in url_list:
+		webbrowser.open_new_tab(url)
+		time.sleep(1)
+
+if __name__ == "__main__":
+    main()
