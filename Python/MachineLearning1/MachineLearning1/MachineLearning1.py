19ae81e081b338cb77485b8cf52198de639027c0
piercuta <pierre.courteille@gmail.com>
2019-05-13 13:35:14 +0200

    add AngularQuickStart porject

--- Python/MachineLearning1/MachineLearning1/MachineLearning1.py
+++ Python/MachineLearning1/MachineLearning1/MachineLearning1.py
@@ -8,3 +8,16 @@ col_types = df.dtypes;
 dims = df.shape;
 print (col_types);
 print (dims);
+print (df.head(5));
+print(df.ftypes);
+print(df.Aspect.nunique())
+print(df.Id.nunique())
+
+print(df.head(5))
+
+df_first_five = df.head(5)
+boolex = df_first_five.Aspect == 51
+print (df_first_five[boolex])
+var = df.count(axis='columns')
+print (var.nunique())
+print(df.describe())
