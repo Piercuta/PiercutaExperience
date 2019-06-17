8603680cf27a20b31dafad5c0e6c1572fb41a892
piercuta <pierre.courteille@gmail.com>
2019-06-17 19:40:44 +0200

    implemetnation of dog class

--- Python/advanced_python/advanced_python/classes/abstract_class.py
+++ Python/advanced_python/advanced_python/classes/abstract_class.py
@@ -33,10 +33,26 @@ class AbstractAnimal(metaclass=ABCMeta):
 Exercise: Implement the Dog class so that the code below runs.
 """
 class Dog(AbstractAnimal):
+   
+    def _init_(self):
+        self._name =''
+        self.alive = True
+        self.species = 'berger allemand'
 
-    pass
+    def getname(self):
+        return self._name
+
+    def setname(self, value):
+        self._name = value
+    
+    name = property(getname,setname)
 
 
+    def make_noise(self):
+        print("wof wof")
+
+    def species(self):
+        return self.species
 
 rex = Dog()
 print(rex.is_alive())
